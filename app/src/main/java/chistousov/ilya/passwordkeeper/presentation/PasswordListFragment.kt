ad029abfcc8552e17d9d76b9f6f6d8d6eef99c07
kw4s1k <kw4s1k2@gmail.com>
2023-05-19 21:25:46 +0300

    creating layouts

--- app/src/main/java/chistousov/ilya/passwordkeeper/presentation/PasswordListFragment.kt
+++ app/src/main/java/chistousov/ilya/passwordkeeper/presentation/PasswordListFragment.kt
@@ -1,15 +1,11 @@
-package chistousov.ilya.passwordkeeper
+package chistousov.ilya.passwordkeeper.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import chistousov.ilya.passwordkeeper.R
 
 /**
  * A simple [Fragment] subclass.
@@ -24,8 +20,6 @@ class PasswordListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -51,8 +45,7 @@ class PasswordListFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             PasswordListFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
